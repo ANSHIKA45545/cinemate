6bf775f6f301654e014a3f08bbb93c8110b46905
livewithanshika <yanshika216@gmail.com>
2025-02-28 14:53:41 +0530

    updated content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,7 +58,7 @@ const Header=() =>{
     <div className="space-y-1 px-2 pb-3 pt-2">
       <Link to="/" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</Link>
       <Link to="movies/popular" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Popular</Link>
-      <Link to="movies/top" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Top-rated</Link>
+      <Link to="movies/top-rated" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Top-rated</Link>
       <Link to="movies/upcoming"  className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Upcoming</Link>
     </div>
   </div>
